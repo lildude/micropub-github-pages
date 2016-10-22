d51f90582cbb714833fd6ec566e9a0622c3ac1e3
Colin Seymour <lildood@gmail.com>
2016-10-22 12:51:54 +0100

    Fix response from test stub

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,12 +10,12 @@ WebMock.disable_net_connect!(:allow_localhost => true)
 def stub_token
   stub_request(:get, "http://example.com/micropub/token").
     with(headers: {'Authorization'=>'Bearer 1234567890', 'Content-Type'=>'application/x-www-form-urlencoded'}).
-    to_return(status: 200, body: URI.encode_www_form([
+    to_return(status: 200, body: URI.encode_www_form({
       :me => "https://testsite.example.com",
       :issued_by => "http://localhost:4567/micropub/token",
       :client_id => "http://testsite.example.com",
       :issued_at => "123456789",
       :scope => "post",
       :nonce => "0987654321"
-      ]))
+      }))
 end
