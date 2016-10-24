a3c20804242c7cae828aebe630a20881a47cfd23
Colin Seymour <lildood@gmail.com>
2016-10-24 18:22:38 +0100

    Add two new request stubs and make things uniform

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,8 +9,8 @@ WebMock.disable_net_connect!(:allow_localhost => true)
 
 def stub_token
   stub_request(:get, "http://example.com/micropub/token").
-    with(headers: {'Authorization'=>'Bearer 1234567890', 'Content-Type'=>'application/x-www-form-urlencoded'}).
-    to_return(status: 200, body: URI.encode_www_form({
+    with(:headers => {'Authorization'=>'Bearer 1234567890', 'Content-Type'=>'application/x-www-form-urlencoded'}).
+    to_return(:status => 200, :body => URI.encode_www_form({
       :me => "https://testsite.example.com",
       :issued_by => "http://localhost:4567/micropub/token",
       :client_id => "http://testsite.example.com",
@@ -18,4 +18,14 @@ def stub_token
       :scope => "post",
       :nonce => "0987654321"
       }))
+end
+
+def stub_get_github_request
+  stub_request(:get, "https://api.github.com/repos/lildude/testsite.github.io").
+    to_return(:status => 200, :body => "{ json here }")
+end
+
+def stub_put_github_request
+  stub_request(:put, /api.github.com\/repos\/lildude\/testsite.github.io\/contents\/_posts\/[0-9]{4}-[0-9]{2}-[0-9]{2}-[a-z0-9].*\.md/).
+    to_return(:status => 201, :body => "{ json here }")
 end
