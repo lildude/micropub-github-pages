9fbf0598853c68a0347bd528602ba8cf20a7bc1e
Colin Seymour <lildood@gmail.com>
2016-10-25 11:05:04 +0100

    Use our own repo name in testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,11 +21,11 @@ def stub_token
 end
 
 def stub_get_github_request
-  stub_request(:get, "https://api.github.com/repos/lildude/testsite.github.io").
+  stub_request(:get, "https://api.github.com/repos/lildude/micropub-github-pages").
     to_return(:status => 200, :body => "{ json here }")
 end
 
 def stub_put_github_request
-  stub_request(:put, /api.github.com\/repos\/lildude\/testsite.github.io\/contents\/_posts\/[0-9]{4}-[0-9]{2}-[0-9]{2}-[a-z0-9].*\.md/).
+  stub_request(:put, /api.github.com\/repos\/lildude\/micropub-github-pages\/contents\/_posts\/[0-9]{4}-[0-9]{2}-[0-9]{2}-[a-z0-9].*\.md/).
     to_return(:status => 201, :body => "{ json here }")
 end
