82d77bdf94e02d56a6f54760c630adff11e4c691
Colin Seymour <lildood@gmail.com>
2016-10-28 15:03:13 +0100

    Update tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -36,16 +36,19 @@ class MainAppTest < Minitest::Test
     assert last_response.body.include?('404: Not Found')
   end
 
-  def test_unauthorized_if_no_auth_header_or_access_token
+  def test_unauthorized_if_no_auth_header_or_access_token_and_error_body_is_json
     post '/micropub/testsite'
     assert last_response.unauthorized?
-    assert last_response.body.include?('401: Unauthorized')
+    assert JSON.parse(last_response.body)
+    assert last_response.body.include?('unauthorized')
   end
 
   def test_authorized_if_access_token_response_has_no_scope
     stub_noscope_token_response
     post '/micropub/testsite', nil, {"HTTP_AUTHORIZATION" => "Bearer 1234567890"}
     assert last_response.unauthorized?
+    assert JSON.parse(last_response.body)
+    assert last_response.body.include?('insufficient_scope')
   end
 
   def test_authorized_if_auth_header_and_no_action
@@ -53,7 +56,8 @@ class MainAppTest < Minitest::Test
     post '/micropub/testsite', nil, {"HTTP_AUTHORIZATION" => "Bearer 1234567890"}
     refute last_response.unauthorized?
     assert last_response.bad_request?
-    assert last_response.body.include?('400: invalid_request')
+    assert JSON.parse(last_response.body)
+    assert last_response.body.include?('invalid_request')
   end
 
   def test_authorized_if_access_token_query_param_and_no_action
@@ -61,10 +65,11 @@ class MainAppTest < Minitest::Test
     post '/micropub/testsite', :access_token => "1234567890"
     refute last_response.unauthorized?
     assert last_response.bad_request?
-    assert last_response.body.include?('400: invalid_request')
+    assert JSON.parse(last_response.body)
+    assert last_response.body.include?('invalid_request')
   end
 
-  def test_new_entry_with_syndication_everything_and_unrecognised_params
+  def test_new_note_with_syndication_everything_and_unrecognised_params
     stub_token
     stub_get_github_request
     stub_put_github_request
@@ -81,16 +86,16 @@ class MainAppTest < Minitest::Test
       }, {"HTTP_AUTHORIZATION" => "Bearer 1234567890"})
     assert last_response.created?, "Expected 201 but got #{last_response.status}"
     assert last_response.header.include?('Location'), "Expected 'Location' header, but got #{last_response.header}"
-    assert_equal "---\nlayout: note\ntags: tag1, tag2\npermalink: this-is-the-content-slug\ndate: #{now}\n---\n\nThis is the content", last_response.body
+    assert_equal "---\nlayout: note\ntags: tag1, tag2\npermalink: this-is-the-content-slug\ndate: #{now}\n---\n\This is the content", last_response.body
   end
 
-  def test_new_note
+  def test_new_entry
     stub_token
     stub_get_github_request
     stub_put_github_request
     post('/micropub/testsite', {
       :h => "entry",
-      :title => "This is a 😍 Post!!",
+      :name => "This is a 😍 Post!!",
       :content => "This is the content",
       :category => ["tag1", "tag2"],
       "mp-syndicate-to" => "https://myfavoritesocialnetwork.example/lildude"
