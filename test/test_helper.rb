b7a87ac0989ae84170f4b78d96f4f52f3ff5865f
Colin Seymour <lildood@gmail.com>
2017-01-28 16:55:15 +0000

    Make stub urls more generic

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -56,16 +56,16 @@ def stub_put_github_request
 end
 
 def stub_get_photo
-  stub_request(:get, /scontent.cdninstagram.com\/t51.2885-15\/e35\/12716713_162835967431386_291746593_n.jpg/).
+  stub_request(:get, /.*instagram.*\/t51.2885-15\/e35\/\d+_\d+_\d+_n.jpg/).
     to_return(:status => 200, :body => open("test/fixtures/photo.jpg", "rb"))
 end
 
 def stub_non_existant_github_file
-  stub_request(:get, /api.github.com\/repos\/lildude\/micropub-github-pages\/contents\/.*\/12716713_162835967431386_291746593_n.jpg/).
+  stub_request(:get, /api.github.com\/repos\/lildude\/micropub-github-pages\/contents\/.*\/\d+_\d+_\d+_n.jpg/).
     to_return(:status => 404, :body => '404 - Not Found')
 end
 
 def stub_existing_github_file
-  stub_request(:get, /api.github.com\/repos\/lildude\/micropub-github-pages\/contents\/.*\/12716713_162835967431386_291746593_n.jpg/).
+  stub_request(:get, /api.github.com\/repos\/lildude\/micropub-github-pages\/contents\/.*\/\d+_\d+_\d+_n.jpg/).
     to_return(:status => 200, :body => '{  "sha": "3d21ec53a331a6f037a91c368710b99387d012c1" }') # We don't need the rest of the info from the response, yet.
 end
