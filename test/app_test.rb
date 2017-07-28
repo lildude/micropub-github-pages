e827ccf2c394eb8d21cd329644b54a07ce3d83a5
Colin Seymour <lildood@gmail.com>
2017-07-28 12:34:14 +0100

    Setup helper on each test

--- test/app_test.rb
+++ test/app_test.rb
@@ -21,10 +21,15 @@ class MainAppTest < Minitest::Test
     Class.new { include AppHelpers }
   end
 
+  def setup
+    @helper = helpers.new
+  end
+
   #### ----- AppHelpers unit tests ----- ####
   def test_verify_token_valid
     stub_token
-    result = helpers.new.verify_token("Bearer 1234567890")
+    @helper.instance_variable_set(:@access_token, "1234567890")
+    result = @helper.verify_token
     assert result.include? :me
     assert result.include? :issued_by
     assert result.include? :client_id
@@ -34,13 +39,13 @@ class MainAppTest < Minitest::Test
   def test_verify_token_no_scope
     skip('TODO: not yet implemented')
     stub_token
-    result = helpers.new.verify_token("Bearer 1234567890")
+    result = @helper.verify_token("Bearer 1234567890")
   end
 
   def test_verify_token_no_me
     skip('TODO: not yet implemented')
     stub_token
-    result = helpers.new.verify_token("Bearer 1234567890")
+    result = @helper.verify_token("Bearer 1234567890")
   end
 
   def test_publish_post
@@ -57,18 +62,18 @@ class MainAppTest < Minitest::Test
 
   def test_jekyll_post_to_json
     content = "---\nlayout: post\ntags:\n- tag1\n- tag2\npermalink: \"/2017/07/foo-bar\"\ndate: 2017-07-22 10:56:22 +0100\n---\nThis is the content"
-    assert_equal '{"type":["h-entry"],"properties":{"published":["2017-07-22 10:56:22 +0100"],"content":["This is the content"],"slug":["/2017/07/foo-bar"],"category":["tag1","tag2"]}}', helpers.new.jekyll_post_to_json(content)
+    assert_equal '{"type":["h-entry"],"properties":{"published":["2017-07-22 10:56:22 +0100"],"content":["This is the content"],"slug":["/2017/07/foo-bar"],"category":["tag1","tag2"]}}', @helper.jekyll_post_to_json(content)
   end
 
   def test_create_slug
-    assert_equal "this-is-a-slug", helpers.new.create_slug({slug: "this-is-a-slug"})
-    assert_equal "this-is-a-name-slug", helpers.new.create_slug({name: "This is a name 😜 Slug"})
-    assert_equal "35782", helpers.new.create_slug({published: "2017-07-02 02:56:22 -0700"})
+    assert_equal "this-is-a-slug", @helper.create_slug({slug: "this-is-a-slug"})
+    assert_equal "this-is-a-name-slug", @helper.create_slug({name: "This is a name 😜 Slug"})
+    assert_equal "35782", @helper.create_slug({published: "2017-07-02 02:56:22 -0700"})
   end
 
   def test_slugify
-    assert_equal "this-is-text", helpers.new.slugify('this is text')
-    assert_equal "this-is-1234-no-emoji-or-punc", helpers.new.slugify('this is 🍎 1234 no emoji ! or punc')
+    assert_equal "this-is-text", @helper.slugify('this is text')
+    assert_equal "this-is-1234-no-emoji-or-punc", @helper.slugify('this is 🍎 1234 no emoji ! or punc')
   end
 
   def test_create_permalink
@@ -77,11 +82,12 @@ class MainAppTest < Minitest::Test
       slug: "foo-bar",
       published: "2017-07-02 02:56:22 -0700",
     }
-    assert_equal "/2017/07/7/02/2/17/02/56/22/foo-bar", helpers.new.create_permalink(params)
+    assert_equal "/2017/07/7/02/2/17/02/56/22/foo-bar", @helper.create_permalink(params)
   end
 
   def test_syndicate_to
-    skip('TODO: not yet implemented')
+    @helper.instance_variable_set(:@access_token, "1234567890")
+    assert_equal "", @helper.syndicate_to
   end
 
   def test_process_params
@@ -97,14 +103,14 @@ class MainAppTest < Minitest::Test
   end
 
   def test_post_type
-    assert_equal :article, helpers.new.post_type({h: "entry", name: "foo", content: "foo"})
-    assert_equal :reply, helpers.new.post_type({h: "entry", in_reply_to: "foo"})
-    assert_equal :repost, helpers.new.post_type({h: "entry", repost_of: "foo"})
-    assert_equal :bookmark, helpers.new.post_type({h: "entry", bookmark_of: "foo"})
-    assert_equal :note, helpers.new.post_type({h: "entry", content: "foo"})
-    assert_equal :dump_all, helpers.new.post_type({h: "entry", ano: "foo"})
-    assert_equal :event, helpers.new.post_type({h: "event", content: "foo"})
-    assert_equal :cite, helpers.new.post_type({h: "cite", content: "foo"})
+    assert_equal :article, @helper.post_type({h: "entry", name: "foo", content: "foo"})
+    assert_equal :reply, @helper.post_type({h: "entry", in_reply_to: "foo"})
+    assert_equal :repost, @helper.post_type({h: "entry", repost_of: "foo"})
+    assert_equal :bookmark, @helper.post_type({h: "entry", bookmark_of: "foo"})
+    assert_equal :note, @helper.post_type({h: "entry", content: "foo"})
+    assert_equal :dump_all, @helper.post_type({h: "entry", ano: "foo"})
+    assert_equal :event, @helper.post_type({h: "event", content: "foo"})
+    assert_equal :cite, @helper.post_type({h: "cite", content: "foo"})
   end
 
   #### ---- Integration tests ---- ####
