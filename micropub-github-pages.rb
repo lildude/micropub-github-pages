df93817bd53fac57285d8853a90959f2261b5ab1
Colin Seymour <lildood@gmail.com>
2017-08-02 10:40:27 +0100

    Set frozen_string_literal: true
    ...cos future-proofing is better now than later

--- micropub-github-pages.rb
+++ micropub-github-pages.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 # Spec: https://www.w3.org/TR/micropub
 require 'rubygems'
 require 'bundler/setup'
--- test/app_test.rb
+++ test/app_test.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'simplecov'
 require 'coveralls'
 SimpleCov.formatters = [
