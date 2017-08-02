77e2bbf797f6cbaa7365e727526c11e32e9293e6
Colin Seymour <lildood@gmail.com>
2017-08-02 11:48:08 +0100

    Make stub url regexes less specific

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -49,12 +49,12 @@ def stub_unauthed_token
 end
 
 def stub_get_github_request
-  stub_request(:get, 'https://api.github.com/repos/lildude/micropub-github-pages')
+  stub_request(:get, %r{api.github.com/repos/.*/micropub-github-pages})
     .to_return(status: 200, body: '{ json here }')
 end
 
 def stub_put_github_request
-  stub_request(:put, %r{api.github.com/repos/lildude/micropub-github-pages/contents/.*/.*\.[a-z]{2,}})
+  stub_request(:put, %r{api.github.com/repos/.*/contents/.*\.[a-z]{2,}})
     .to_return(status: 201, body: '{ json here }')
 end
 
@@ -69,12 +69,12 @@ def stub_cant_get_photo
 end
 
 def stub_non_existant_github_file
-  stub_request(:get, %r{api.github.com/repos/lildude/micropub-github-pages/contents/.*/\d+_\d+_\d+_n.jpg})
+  stub_request(:get, %r{api.github.com/.*/contents/.*/\d+_\d+_\d+_n.jpg})
     .to_return(status: 404, body: '404 - Not Found')
 end
 
 def stub_existing_github_file
-  stub_request(:get, %r{api.github.com/repos/lildude/micropub-github-pages/contents})
+  stub_request(:get, %r{api.github.com/repos/.*/.*/contents})
     .to_return(status: 200, headers: { 'Content-Type' => 'application/json' }, body: JSON.generate(sha: 'd735c3364cacbda4a9631af085227ce200589676',
                                                                                                    content: 'LS0tDQpsYXlvdXQ6IHBvc3QNCnRpdGxlOiAgVGhpcyBpcyBhIFRlc3QgUG9zdA0KZGF0ZTogICAyMDE3LTAxLTIwIDEwOjAxOjQ4DQp0YWdzOiANCi0gZm9vIA0KLSBiYXINCnBlcm1hbGluazogLzIwMTcvMDEvdGhpcy1pcy1hLXRlc3QtcG9zdA0KLS0tDQoNClRoaXMgaXMgYSB0ZXN0IHBvc3Qgd2l0aDoNCg0KLSBUYWdzLA0KLSBhIHBlcm1hbGluaw0KLSBhbmQgc29tZSAqKmJvbGQqKiBhbmQgX19pdGFsaWNfXyBtYXJrZG93bg=='))
 end
