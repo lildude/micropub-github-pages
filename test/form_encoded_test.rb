0bf47e57a9a5e9d0c3080fa250408afca50ce161
Colin Seymour <colin@symr.io>
2020-11-03 17:37:58 +0000

    Add a todo test

--- test/form_encoded_test.rb
+++ test/form_encoded_test.rb
@@ -41,6 +41,15 @@ class FormEncoded < Minitest::Test
     refute JSON.parse(last_response.body)['syndicate-to'].empty?
   end
 
+  # I think Sunlit performs a media-endpoint query IIRC
+  def test_get_media_endpoint
+    skip 'TODO: not yet implemented'
+    get '/micropub/testsite?q=media-endpoint', nil, 'HTTP_AUTHORIZATION' => 'Bearer 1234567890'
+    assert last_response.ok?
+    parse_body = JSON.parse(last_response.body)
+    assert_equal parse_body['media-endpoint'], 'http://example.org/micropub/testsite/media'
+  end
+
   def test_get_source
     stub_github_search
     stub_get_github_request
