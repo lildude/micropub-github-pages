e9b8fc262f917319d200f15f0a8a67bc5392f8fb
Colin Seymour <colin@symr.io>
2021-01-01 11:55:16 +0000

    Use default branch if pages isn't enabled
    This is needed for peeps who use Netlify like me.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -85,8 +85,9 @@ module AppHelpers
   # Files should be an array of path and base64 encoded content
   def commit_to_github(files, type)
     # Verify the repo exists
-    client.repository?(github_repo)
-    ref = "heads/#{client.pages(github_repo).source.branch}"
+    repo = client.repository(github_repo)
+    branch = repo.has_pages ? client.pages(github_repo).source.branch : repo.default_branch
+    ref = "heads/#{branch}"
     sha_latest_commit = client.ref(github_repo, ref).object.sha
     sha_base_tree = client.commit(github_repo, sha_latest_commit).commit.tree.sha
 
